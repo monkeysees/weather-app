44b87b2d397c4a6ba631c25a2f0b095454f7e34d
Islambeg <islambeg@protonmail.com>
2022-07-13 18:36:57 +0300

    Show current location weather when current location is selected

--- src/features/location-search/components/LocationSearch/LocationSearch.tsx
+++ src/features/location-search/components/LocationSearch/LocationSearch.tsx
@@ -1,10 +1,25 @@
 import React, { useState } from "react";
+import { useUserDispatch } from "../../../../providers/UserProvider";
 import { Button, Ellipse, Icon } from "../../../../components";
 import SearchModal from "../SearchModal";
 import "./LocationSearch.scss";
 
 function LocationSearch() {
+  const userDispatch = useUserDispatch();
   const [isShowModal, setIsShowModal] = useState(false);
+
+  function handleCurrentLocationSelected() {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((pos) => {
+        userDispatch({
+          type: "change-location",
+          location: {
+            coords: { lat: pos.coords.latitude, lon: pos.coords.longitude },
+          },
+        });
+      });
+    }
+  }
 
   return (
     <section className="locationSearch">
@@ -19,7 +34,10 @@ function LocationSearch() {
         Search for places
       </Button>
       <Ellipse className="locationSearch__currentLocation">
-        <Button className="locationSearch__currentLocation__btn">
+        <Button
+          className="locationSearch__currentLocation__btn"
+          onClick={() => handleCurrentLocationSelected()}
+        >
           <Icon type="gps" className="locationSearch__currentLocation__icon" />
         </Button>
       </Ellipse>
